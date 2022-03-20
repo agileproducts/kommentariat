650a6d85fefda19d8f489538548ea811c2d18c86
Stephen Cornelius <stephencornelius@Stephens-MacBook-Air.local>
2022-03-20 18:15:50 +0000

    SC - CRUD

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,4 +1,5 @@
 class ConversationsController < ApplicationController
+
   def index
     @conversations = Conversation.all
   end
@@ -6,4 +7,46 @@ class ConversationsController < ApplicationController
   def show
     @conversation = Conversation.find(params[:id])
   end
+
+  def new
+    @conversation = Conversation.new
+  end
+
+  def create
+    @conversation = Conversation.new(conversation_params)
+
+    if @conversation.save
+      redirect_to @conversation
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def edit
+    @conversation = Conversation.find(params[:id])
+  end
+
+  def update
+    @conversation = Conversation.find(params[:id])
+
+    if @conversation.update(conversation_params)
+      redirect_to @conversation
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @conversation = Conversation.find(params[:id])
+    @conversation.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+  def conversation_params
+    params.require(:conversation).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "conversations#index"
-  get "/conversations", to: "conversations#index"
-  get "/conversations/:id", to: "conversations#show"
+  resources :conversations
 
   # Defines the root path route ("/")
   # root "articles#index"
