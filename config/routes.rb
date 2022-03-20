e68d562919c3a34c9fe7be297200f9153c96544b
Stephen Cornelius <stephencornelius@Stephens-MacBook-Air.local>
2022-03-20 17:04:57 +0000

    SC - added model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "conversations#index"
   get "/conversations", to: "conversations#index"
+  get "/conversations/:id", to: "conversations#show"
 
   # Defines the root path route ("/")
   # root "articles#index"
