dd4c62088eccdded16cf519d39a36408b2518e1f
Stephen Cornelius <stephencornelius@stephens-air.home>
2022-03-29 21:23:51 +0100

    SC - left on failing markdown integration test

---
+++ test/integration/render_conversation_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class RenderConversationTest < ActionDispatch::IntegrationTest
+  test "create and display conversation" do
+    get "/conversations/new"
+    assert_response :success
+
+    post "/conversations", 
+      params: { conversation: {title: "Test rendering", body: "I *am* **some** [markdown](link)"} }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "p", 'I <em>am</em> <strong>some</strong> <a href="link">markdown</a>', "markdown not rendering as HTML"
+  end
+end
