9e36cd8db230ab1f588bf88b09f6b8d4820c3e3f
Stephen Cornelius <stephencornelius@stephens-air.home>
2022-04-08 23:04:17 +0100

    SC - create comment sort of working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    @conversation = Conversation.find(params[:conversation_id])
+    @comment = @conversation.comments.create(comment_params)
+    redirect_to conversation_path(@conversation)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
 end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @conversation = conversations(:two)
+    @comment_params = {commenter: "Barney", body: "Lorem ipsum"}
+  end
+
+  teardown do
+    Rails.cache.clear
+  end
+
+  test "it should create a comment" do
+    starting_count = Comment.count
+    post "#{conversation_path(@conversation)}/comments", params: { comment: @comment_params } 
+    assert_redirected_to conversation_path(@conversation)
+    assert_equal starting_count+1, Comment.count
+  end
+
 end
